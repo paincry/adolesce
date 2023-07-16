7fae61cf836654c25b3d6ba102c0b10ebf56b115
Mr.pain <1477871185@qq.com>
2023-07-16 21:05:32 +0800

    二分法的算法练习

---
+++ algorithm/src/algorithm/BinarySearch.java
@@ -0,0 +1,20 @@
+package algorithm;
+
+public class BinarySearch {
+
+    public static int binarySearchBasic(int[] a, int target) {
+        int i = 0;
+        int j = a.length - 1;
+        while (i <= j) {
+            int m = (i + j) / 2;
+            if (target < a[m]) {
+                j = m - 1;
+            } else if (target > a[m]) {
+                i = m + 1;
+            } else {
+                return m;
+            }
+        }
+        return -1;
+    }
+}
---
+++ algorithm/src/algorithm/BinarySearchTest.java
@@ -0,0 +1,10 @@
+package algorithm;
+
+public class BinarySearchTest {
+
+    public void main() {
+        int []a={1,4,11,23,33,36,39,40,55,90};
+
+    }
+
+}
